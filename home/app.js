b343fe2d1896201166ced7baa0e3d57cc9a0fbeb
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-02-20 16:38:30 +0800

    feat: added a simple page guard to home page

--- home/app.js
+++ home/app.js
@@ -1,6 +1,12 @@
 import {getCurrentUser, logOut} from '../auth.js'
 
+
 const user = getCurrentUser();
+
+if(!user){
+    alert("No user is currently logged in. Please log in to continue.");
+    document.location.href = "../index.html";
+}
 
 document.getElementById("userInfo").textContent = `Username: ${user.username} | Email: ${user.email}`;
 
